3ad0c751814b9f4fd3f75bc345d6b3d9faac66f7
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-01-28 14:45:16 +0600

    completed the using vanilla js and dom by myself

--- js/utility.js
+++ js/utility.js
@@ -1,48 +1,51 @@
-function hideElementById(elementId) {
+//hide pages
+function hideElementById (elementId){
   const element = document.getElementById(elementId);
-  element.classList.add("hidden");
+  const hideElement = element.classList.add('hidden');
+}
+//show pages 
+function showElementById (elementId){
+  const element = document.getElementById(elementId);
+  const showElement = element.classList.remove('hidden');
+}
+//generate the random alphabet
+function getRandomAlphabet(){
+  const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabets = alphabetString.split("");
+  const randomNumber = Math.random() * 25;
+  const index = Math.round(randomNumber);
+  const alphabet = alphabets[index];
+  // console.log(alphabet);
+  return alphabet;
 }
 
-function showElementById(elementId) {
-  const element = document.getElementById(elementId);
-  element.classList.remove("hidden");
-}
-
-function setBackgroundColorById(elementId) {
-  const element = document.getElementById(elementId);
-  element.classList.add("bg-orange-400");
-}
-
-function removeBackgroundColorById(elementId) {
-  const element = document.getElementById(elementId);
-  element.classList.remove("bg-orange-400");
-}
-
-function getTextElementValueById(elementId) {
+//updated the life and the score 
+function getTextValueById (elementId){
   const element = document.getElementById(elementId);
   const elementValueText = element.innerText;
   const value = parseInt(elementValueText);
   return value;
 }
+// set the number in the updated life and the score 
 
-function setTextElementValueById(elementId, value) {
+function setTheUpdatedScore (elementId, value){
   const element = document.getElementById(elementId);
-  element.innerText = value;
+  element.innerText= value;
+}
+//add the background color
+function setBackGroundColorById(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.add('bg-orange-400');
+}
+//remove the background color
+
+function removeBackGroundColorById(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.remove('bg-orange-400');
 }
 
-function getElementTextById(elementId) {
+function getElementTextById (elementId){
   const element = document.getElementById(elementId);
-  const text = element.innerText;
-  return text;
-}
-
-function getARandomAlphabet() {
-  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
-  const alphabets = alphabetString.split("");
-
-  const randomNumber = Math.random() * 25;
-  const index = Math.round(randomNumber);
-
-  const alphabet = alphabets[index];
-  return alphabet;
+   const text = element.innerText;
+   return text;
 }
